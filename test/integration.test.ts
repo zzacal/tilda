d604bc1e128a80b502c11e0de430b7c0f5ace87b
Zac Zacal <jizacal@gmail.com>
2021-05-29 19:52:50 -0700

    Added supertest

---
+++ test/integration.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+import Server from "../src/server";
+
+describe("server", () => {
+  const server = new Server();
+  const app = server.express;
+
+  const userName = "Mitch Hedberg";
+  const path = "/user";
+  const params = {"id": "123"};
+  const body = {};
+  const val = { userName };
+  beforeAll((done) => {
+    request(app)
+      .post("/mock")
+      .send({
+        path,
+        params,
+        body,
+        val
+      })
+      .expect(200)
+      .then((response) => {
+        expect(response.body?.val?.userName).toBe(userName);
+        done();
+      });
+  });
+
+  it("server#fetch returns val with the right path, params, and body", (done) => {
+    request(app)
+    .get(`${path}?id=123`)
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .then((response) => {
+      expect(response.body?.userName).toBe(userName);
+      done();
+    });
+  });
+});
