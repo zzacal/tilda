57f1056867a0c458bb77bce1355d85a14208fb4c
zzacal <jizacal@gmail.com>
2021-06-14 13:28:40 -0700

    Stub out store duplicate prevention

--- src/cacher/store.ts
+++ src/cacher/store.ts
@@ -5,22 +5,66 @@ import { Record } from '../types/record'
 
 export default class Store {
   private cache: Record[] = [];
-  constructor () {
+  constructor() {
     this.cache = []
   }
 
-  add (setup: MockSetup): Record {
+  private getRecord(path: string, params?: any, body?: any) {
+    return this.cache
+      .filter((r) => r.path === path)[0]
+      // .filter((r) => _.isEqual(r.params, params))
+      // .filter((r) => _.isEqual(r.body, body))
+  }
+
+  add(setup: MockSetup): Record {
     const { request, response } = setup
-    const record = { path: request.path, params: request.params, body: request.body, response: response }
-    this.cache.push(record)
+    const record = {
+      path: request.path,
+      params: request.params,
+      body: request.body,
+      response: response
+    }    
+    
+    const result = this.getRecord(setup.request.path, setup.request.params, setup.request.body);
+
+    if(result) {
+      result.response = setup.response;
+    } else {
+      this.cache.push(record)
+    }
+
     return record
   }
 
-  get (path: string, params?: any, body?: any): MockResponse {
-    return this.cache
-      .filter((r) => r.path === path)
-      .filter((r) => _.isEqual(r.params, params))
-      .filter((r) => _.isEqual(r.body, body))[0]?.response
+  get(path: string, params?: any, body?: any): MockResponse | undefined {
+    console.log(`path: ${path}`)
+    console.log(`params: ${JSON.stringify(body)}`)
+    console.log(`body: ${JSON.stringify(body)}`)
+    console.log(`cache:\n ${JSON.stringify(this.cache)}`)
+
+    const result = this.getRecord(path, params, body);
+
+    if (result) {
+      return result.response;
+    } else {
+      console.log("Warning: No setup found. Mock it.", `
+        curl --location --request POST 'localhost:5111/mock' \\
+        --header 'Content-Type: application/json' \\
+        --data-raw '{
+            "request": {
+                "path": "${path}",
+                "params": ${JSON.stringify(params)},
+                "body": ${JSON.stringify(body)}
+            },
+            "response": {
+                "type": "obj",
+                "status": "200",
+                "body": {}
+            }
+        }'
+      `)
+    }
+    return;
   }
 
   // update(response: any, path: string, params?: any, body?: any): void {}
